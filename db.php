82d5ce8900d279c957d07c8dd830888b5e99fb65
SOCIETY REDRUG <agsjsjx@gmail.com>
2026-08-02 18:11:56 +0400

    new DB

--- db.php
+++ db.php
@@ -1,18 +1,13 @@
 <?php
-// Configuration (.env)
-$host = getenv('DB_HOST');
-$db   = getenv('DB_NAME');
-$user = getenv('DB_USER'); 
-$pass = getenv('DB_PASS');    
+$host = getenv('DB_HOST') ?: 'localhost';
+$db   = getenv('DB_NAME') ?: 'caspsoof_caspian_db';
+$user = getenv('DB_USER') ?: 'caspsoof_redrug';
+$pass = getenv('DB_PASS') ?: 'redrugsociety$0';
 
-// CONNECTION
 $conn = new mysqli($host, $user, $pass, $db);
 
-// Bağlantı xətasının yoxlanılması
 if ($conn->connect_error) {
-    die("Verilənlər bazasına bağlantı xətası: " . $conn->connect_error);
+    die("Bazaya qoşulma xətası: " . $conn->connect_error);
 }
-
-// Character set configuration
 $conn->set_charset("utf8mb4");
 ?>
