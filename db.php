46d5515c15a45ef198c19f6ea99d4d017fa4913a
SOCIETY REDRUG <agsjsjx@gmail.com>
2026-08-02 18:50:34 +0400

    Git updated

--- db.php
+++ db.php
@@ -4,10 +4,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$host = getenv('DB_HOST');
-$db   = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
+$host = $_ENV['DB_HOST'] ?? 'localhost';
+$db   = $_ENV['DB_NAME'] ?? '';
+$user = $_ENV['DB_USER'] ?? '';
+$pass = $_ENV['DB_PASS'] ?? '';
 
 $conn = new mysqli($host, $user, $pass, $db);
 
